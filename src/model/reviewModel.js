fe74935b45d0a681c3dc5ad9e8b8974754a8d220
Soumyaranjanp68 <soumyaranjanp68@gmail.com>
2023-01-23 12:55:48 +0530

    review

---
+++ src/model/reviewModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+
+const reviewSchema = new mongoose.Schema({
+
+    bookId: {
+        type: ObjectId,
+        require: true,
+        ref: "bookData",
+        trim: true
+    },
+    reviewedBy: {
+        type: String,
+        require: true,
+        default: 'Guest',
+        trim: true,
+        value:{
+            type: String,
+            trim: true
+        }
+    },
+    reviewedAt: {
+        type: Date,
+        require: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    review: {
+        type: String,
+        trim: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+
+
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('reviewData', reviewSchema)
